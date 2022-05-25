38df6f919b19a4c4dfd36cab8b436b7adf0d8c99
jyunmitch <j.yunmitchell@entando.com>
2022-05-25 10:47:56 -0500

    first commit

---
+++ ui/widgets/conference/detailsWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,9 @@
+const conferenceApiGetResponse = {
+  id: -469,
+  name:
+    'Qui ea qui voluptas tempora. Debitis soluta et omnis aliquam laboriosam unde laborum. Et quis nemo consequatur est in ab. Et odit optio nesciunt eius et. Tempora eos illum qui numquam sed aut. Et ea voluptas qui dolore dolor molestias molestiae enim non.',
+  location:
+    'Qui et aspernatur mollitia at consequatur quia. Eos earum repudiandae. Eaque consequuntur reiciendis non excepturi iusto. Dolores nesciunt rem et quia. Voluptatem velit illo sit et.',
+};
+
+export default conferenceApiGetResponse;
---
+++ ui/widgets/conference/tableWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,18 @@
+const conferenceMocks = [
+  {
+    id: -469,
+    name:
+      'Qui ea qui voluptas tempora. Debitis soluta et omnis aliquam laboriosam unde laborum. Et quis nemo consequatur est in ab. Et odit optio nesciunt eius et. Tempora eos illum qui numquam sed aut. Et ea voluptas qui dolore dolor molestias molestiae enim non.',
+    location:
+      'Qui et aspernatur mollitia at consequatur quia. Eos earum repudiandae. Eaque consequuntur reiciendis non excepturi iusto. Dolores nesciunt rem et quia. Voluptatem velit illo sit et.',
+  },
+  {
+    id: -457,
+    name:
+      'Saepe ea ex qui alias reiciendis rerum ipsa. Ipsum aliquid voluptates dignissimos fuga iure aut aut est. Quia neque qui assumenda. Rem doloremque voluptas officia ab laborum dignissimos libero. Velit est eum ut ducimus et illum est. Rerum doloribus sapiente illum.',
+    location:
+      'Error est dolores ratione aliquid occaecati soluta necessitatibus assumenda. Accusantium tempora dolor iusto maxime officia vel iste. Id sapiente exercitationem debitis quidem. Est nihil maiores et iusto quis in quam debitis. Incidunt numquam sed alias. Pariatur sapiente pariatur et.',
+  },
+];
+
+export default conferenceMocks;
